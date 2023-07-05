7ecdb211769bf2425618b94785ee544e9543b47b
nick <grahnick@gmail.com>
2023-07-05 19:50:58 -0400

    added bundle schema type, last commit before sharing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import ipyparallel as ipp
 import json
 import jsonschema
 import pandas as pd
-
+import time
 
 
 # from fhir.resources.patient import Patient
@@ -11,7 +11,7 @@ def list_files_in_path(path):
     file_paths = []
     for root, dirs, files in os.walk(path):
             for file in files:
-                print(os.path.join(root, file))
+                # print(os.path.join(root, file))
                 file_paths.append(os.path.join(root, file))
     return file_paths
 
@@ -34,8 +34,10 @@ def path_to_validated_json(path,schema):
     from jsonschema import validate
     with open(path) as json_file:
         file_data = json.load(json_file)
+        file_data
         try:
             validate(file_data, schema)
+            return file_data
         except jsonschema.exceptions.ValidationError as ex:
             print(ex)
             return ""
@@ -43,16 +45,24 @@ def path_to_validated_json(path,schema):
 def schema_type_match(entity_name, schema_path):
     if entity_name == "patient":
         return path_to_json(schema_path + "/patient.schema.json")
-        # return path_to_json("/home/nick/PycharmProjects/ClusteredDataIngestion/venv/resources/schema/patient.schema.json")
-        # "/home/nick/PycharmProjects/ClusteredDataIngestion/venv/resources/schema/patient.schema.json"
-
     if entity_name == "fhir":
         return path_to_json(schema_path + "/fhir.schema.json")
+    if entity_name == "bundle":
+        return path_to_json(schema_path + "/bundle.schema.json")
 
-    #todo json to df
+    def par_file_read(list_of_paths):
+        task_durations = [4] * 25
+        with ipp.Cluster("mpi", 4) as cluster:
+            cluster.start_cluster()
+            client = cluster.start_and_connect_sync()
+            rc = cluster.connect_client_sync()
+            view = rc[:]
+            view.activate()
+            json_list = map(path_to_json, list_of_paths)
+
+
 
 if __name__ == '__main__':
-    # cluster = ipp.Cluster()
     resource_path = '/home/nick/PycharmProjects/ClusteredDataIngestion/venv/resources'
     # todo parameterize resource path,
     synthea_path = resource_path + "/synthea_sample_data_fhir_r4_nov2021/"
@@ -61,13 +71,12 @@ if __name__ == '__main__':
     # todo parameterize num executors
     num_executors = 4
     executor_path_lists = path_split(path_list, num_executors)
-    # print(executor_path_lists)
 
-    # todo parameterize entity type/schema
 
     json_record = path_to_json(path_list.pop())
     schema_path = resource_path+"/schema"
-    json_schema = schema_type_match("patient", schema_path)
+    json_schema = schema_type_match("bundle", schema_path)
 
-    print(json_schema)
-    pd.read_json()
+    # json_record = path_to_validated_json(path_list.pop(), json_schema)
+    print(json_record)
+
